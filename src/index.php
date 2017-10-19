8db959c4bed02e8d0d32fb2f95bed811e508c9b2
louishrg <vundaboy@gmail.com>
2017-10-19 17:32:26 +0200

    Modif Index.php

--- src/index.php
+++ src/index.php
@@ -1,8 +1,12 @@
 <?php 
+echo "<html><body>";
 
-
-echo "Bonjour Cyrille Grandval <b>";
+echo "<h1> Bonjour Cyrille ! </h1><b><b><b>";
 
 $date = date("d/m/Y");
 $heure = date("H:i");
-Print("Nous sommes le $date et il est $heure");
+Print("Nous sommes le $date et il est $heure <b><b><b>");
+
+
+echo "Merci Pierre 2017 &#9400;";
+echo "</body></html>";
